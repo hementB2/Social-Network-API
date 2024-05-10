60099dfd30c6fd9a580a5fcd56be8bd5c3f4a83c
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-05-09 21:11:13 -0400

    Retrive  user code

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,3 +7,10 @@ const UserController = {
       .then(userData => res.json(userData)) // Send response with user data
       .catch(err => res.status(500).json(err)); // Error handling for server errors
   },
+
+   // 2. Retrieve a user by ID
+   getUserById(req, res) {
+    User.findById(req.params.userId)
+      .then(userData => res.json(userData)) // Send response with user data
+      .catch(err => res.status(500).json(err)); // Error handling for server errors
+  },
