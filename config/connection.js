6c06f96c823747fd018199113234500bad5b47ef
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-05-09 20:40:49 -0400

    adding code to connection.js

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// Importing the Mongoose library for MongoDB interaction
+const mongoose = require('mongoose');
+
+// Connecting to the MongoDB database using the provided URI or defaulting to a local URI
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://localhost:3001', {
+    useNewUrlParser: true, // Use new URL parser
+    useUnifiedTopology: true, // Use new server discovery and monitoring engine
+});
+
+// Exporting the database connection as a module
+module.exports = mongoose.connection;
