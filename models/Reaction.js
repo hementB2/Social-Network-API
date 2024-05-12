c825741be05952f89208035e7368e13f2e4a8195
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-05-11 21:27:34 -0400

    Adding Body Text code

--- models/Reaction.js
+++ models/Reaction.js
@@ -8,3 +8,9 @@ const reactionSchema = new Schema(
       type: Schema.Types.ObjectId, // Mongoose ObjectID type
       default: () => new Types.ObjectId(), // Generate a new ObjectId by default
     },
+    // The body text of the reaction
+    reactionBody: {
+        type: String, // String type
+        required: true, // Reaction body is required
+        maxLength: 280, // Maximum length of 280 characters
+      },
