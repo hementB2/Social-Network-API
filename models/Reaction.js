bef3b321a2ee149abac7ffaa45abcaedd9271307
hementB2 <153564981+hementB2@users.noreply.github.com>
2024-05-11 21:28:04 -0400

    Adding  Username for raction

--- models/Reaction.js
+++ models/Reaction.js
@@ -14,3 +14,14 @@ const reactionSchema = new Schema(
         required: true, // Reaction body is required
         maxLength: 280, // Maximum length of 280 characters
       },
+         // The username of the user who created the reaction
+    username: {
+        type: String, // String type
+        required: true, // Username is required
+      },
+      // The timestamp when the reaction was created
+      createdAt: {
+        type: Date, // Date type
+        default: Date.now, // Default to the current date and time
+        get: timestamp => new Date(timestamp).toLocaleDateString() // Convert date to local date string
+      },
